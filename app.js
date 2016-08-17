019a13502cc18d0cebe7e44b4085cbfde73ee9c7
swhudson <scott.w.hudson@gmail.com>
2016-08-17 14:52:07 -0500

    use scss instead of sass

--- app.js
+++ app.js
@@ -26,7 +26,6 @@ app.use(cookieParser());
 app.use(require('node-sass-middleware')({
   src: path.join(__dirname, 'public'),
   dest: path.join(__dirname, 'public'),
-  indentedSyntax: true,
   sourceMap: true
 }));
 app.use(express.static(path.join(__dirname, 'public')));
