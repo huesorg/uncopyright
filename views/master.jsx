c35f6db588560428de0abfda232e95487270f603
Sam Killermann <sK@hues.xyz>
2016-08-17 17:14:48 -0500

    added mdl to the head

--- views/master.jsx
+++ views/master.jsx
@@ -7,6 +7,9 @@ var MasterLayout = React.createClass({
                 <head>
                     <title>{ this.props.name }</title>
                     <link rel='stylesheet' href='/stylesheets/style.css' />
+                    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
+                    <link rel="stylesheet" href="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.css" />
+                    <script src="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.js"></script>
                 </head>
                 <body>
                     { this.props.children }
