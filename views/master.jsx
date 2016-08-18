7771895644cae45fb8db2a5ae182cace7407a274
Sam Killermann <sK@hues.xyz>
2016-08-18 12:49:58 -0500

    Fix Responsive Layout Bug
    Added viewport meta. Took me WAY too long to figure out that was what
    was needed.

--- views/master.jsx
+++ views/master.jsx
@@ -6,10 +6,11 @@ var MasterLayout = React.createClass({
             <html>
                 <head>
                     <title>{ this.props.name }</title>
-                    <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
-                    <link rel="stylesheet" href="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.css" />
-                    <script src="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.js"></script>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+                    <link rel="stylesheet" href="https://code.getmdl.io/1.2.0/material.indigo-pink.min.css" />
+                    <script defer src="https://code.getmdl.io/1.2.0/material.min.js"></script>
                     <link rel='stylesheet' href='/stylesheets/style.css' />
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
                 </head>
                 <body>
                     <div className="mdl-layout mdl-js-layout">
