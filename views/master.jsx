d2ddc1e84b9ba414b7df07e70d37844ccd6d53ad
Sam Killermann <sK@hues.xyz>
2016-08-17 23:28:07 -0500

    Build UI for Projects Page
    Added a new dependency (react gravatar) that I think will make the
    profile pages fun and easy.

--- views/master.jsx
+++ views/master.jsx
@@ -6,13 +6,26 @@ var MasterLayout = React.createClass({
             <html>
                 <head>
                     <title>{ this.props.name }</title>
-                    <link rel='stylesheet' href='/stylesheets/style.css' />
                     <link rel="stylesheet" href="//fonts.googleapis.com/icon?family=Material+Icons" />
                     <link rel="stylesheet" href="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.css" />
                     <script src="//storage.googleapis.com/code.getmdl.io/1.0.1/material.min.js"></script>
+                    <link rel='stylesheet' href='/stylesheets/style.css' />
                 </head>
                 <body>
-                    { this.props.children }
+                    <div className="mdl-layout mdl-js-layout">
+
+                        <div className="mdl-layout__drawer">
+                            <span className="mdl-layout__title">Uncopyright Movement</span>
+                            <nav className="mdl-navigation">
+                                <a className="mdl-navigation__link" href="./">Home</a>
+                                <a className="mdl-navigation__link" href="/projects">Works</a>
+                                <a className="mdl-navigation__link" href="#">About</a>
+                            </nav>
+                        </div>
+                        <main className="mdl-layout__content">
+                            { this.props.children }
+                        </main>
+                    </div>
                 </body>
             </html>
         )
--- views/projects/index.jsx
+++ views/projects/index.jsx
@@ -1,14 +1,36 @@
 var React = require('react');
 var MasterLayout = require('../master');
+import Gravatar from 'react-gravatar';
 
 var ListItemWrapper = React.createClass({
   render: function() {
     return (
-      <li>
-        {this.props.data.firstName}  {this.props.data.lastName},
-        <a href={"/projects/" + this.props.data._id}>Project Page</a>,
-        Categories: { this.props.data.categories }
-      </li>
+      <div className="work mdl-cell mdl-card mdl-shadow--4dp mdl-cell--3-col mdl-cell--4-col-tablet">
+        <div className="mdl-grid">
+            <figure className="mdl-card__media mdl-cell mdl-cell--6-col">
+                <Gravatar email={this.props.data.email} size={200} className="profile-image" https />
+            </figure>
+            <div className="work--icon mdl-cell mdl-cell--6-col mdl-cell--stretch">
+                <i className={"material-icons " + this.props.data.categories}></i>
+            </div>
+        </div>
+        <div className="mdl-card__title">
+            <h3 className="mdl-card__title-text">
+                {this.props.data.firstName} {this.props.data.lastName} is uncopyrighting {this.props.data.categories}
+            </h3>
+        </div>
+        <div className="mdl-card__supporting-text">
+            {this.props.data.description}
+        </div>
+        <div className="view-work mdl-card__actions mdl-card--border mdl-cell--bottom">
+            <a
+                className="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+                href={"/projects/" + this.props.data._id}>
+                View Work
+            </a>
+        </div>
+
+      </div>
     );
   }
 });
@@ -18,12 +40,10 @@ var ProjectListComponent = React.createClass({
     return (
       <MasterLayout name={ this.props.name }>
 
-        <div>
-          <ul>
+        <div className="mdl-grid wrap">
             {this.props.projects.map(function(project) {
                return <ListItemWrapper key={project.id} data={project}/>;
             })}
-          </ul>
         </div>
 
       </MasterLayout>
