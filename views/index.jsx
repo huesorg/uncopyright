0d95ff5d68f4de13373edf897b37045fe4f865b7
Sam Killermann <sK@hues.xyz>
2016-08-19 16:54:33 -0500

    build and style home page default message

--- views/index.jsx
+++ views/index.jsx
@@ -5,11 +5,25 @@ var IndexComponent = React.createClass({
     render: function() {
         return (
             <MasterLayout name={ this.props.name }>
-              <div>
-                  <h1>Welcome to { this.props.name }</h1>
-                  <p>There are { this.props.projectCount } projects.</p>
-                  <a href="/projects">See Current Projects</a>
-              </div>
+                <div className="welcome">
+                    <div className="welcome__content">
+                        <h1 className="welcome__title">
+                            Welcome to { this.props.name }
+                        </h1>
+                        <p className="welcome__message">
+                            All <span>{ this.props.projectCount }</span> works you find here have been uncopyrighted <span>by their creator(s)</span> and gifted to the commons.
+                        </p>
+                        <a className="mdl-button cta-button mdl-js-button mdl-button--raised mdl-js-ripple-effect" href="/about">
+                            Submit a Work
+                        </a>
+
+                        <a className="mdl-button cta-button mdl-shadow-2dp cta-button--secondary mdl-js-button mdl-js-ripple-effect" href="/about">
+                            Browse All
+                        </a>
+                    </div>
+                </div>
+                <div className="mdl-grid wrap">
+                </div>
             </MasterLayout>
         )
     }
